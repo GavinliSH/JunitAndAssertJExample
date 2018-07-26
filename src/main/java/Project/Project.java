a12e9b12ea67704ac503145c49ee20f1e7902d9a
hhu <hhu@thoughtworks.com>
2018-07-26 12:01:05 +0800

    remove unused code

--- src/main/java/Project/Project.java
+++ src/main/java/Project/Project.java
@@ -1,15 +1,12 @@
 package Project;
 
-import com.sun.istack.internal.NotNull;
-
 public class Project {
-    @NotNull
+    
     private ProjectType projectType;
 
-    @NotNull
     private String projectName;
 
-    public Project(ProjectType projectType, String projectName){
+    public Project(ProjectType projectType, String projectName) {
         this.projectType = projectType;
         this.projectName = projectName;
     }
