c1f1145a6aae21936c76724e87190fbfd43cf7e8
RuipengWang <>
2018-08-04 20:53:26 +0800

    Add test case: should return internal expense type if project is internal --Ruipeng Wang

--- src/test/java/ExpenseService/ExpenseServiceTest.java
+++ src/test/java/ExpenseService/ExpenseServiceTest.java
@@ -1,4 +1,19 @@
 package ExpenseService;
 
-class ExpenseServiceTest {
+import ExpenseService.Exception.UnexpectedProjectTypeException;
+import ExpenseService.Expense.ExpenseType;
+import ExpenseService.Project.Project;
+import ExpenseService.Project.ProjectType;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ExpenseServiceTest {
+    @Test
+    void should_return_internal_expense_type_if_project_is_internal() throws UnexpectedProjectTypeException {
+        Project project = new Project(ProjectType.INTERNAL, "Internal Project");
+        ExpenseType expenseType = ExpenseService.getExpenseCodeByProjectTypeAndName(project);
+
+        assertThat(expenseType).isEqualTo(ExpenseType.INTERNAL_PROJECT_EXPENSE);
+    }
 }
